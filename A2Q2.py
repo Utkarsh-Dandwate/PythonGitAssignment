edbc972eda73a45fe922cda91e1092ea0c473f58
Utkarsh-Dandwate <64021176+Utkarsh-Dandwate@users.noreply.github.com>
2020-06-30 10:25:40 -0400

    Create A2Q2.py
    program to find largest, 2nd largest, smallest and 2nd smallest numbers of the given list

---
+++ A2Q2.py
@@ -0,0 +1,8 @@
+x = [12, 29, 65, 45, 37, 89, 94, 68, 75]
+
+x.sort()
+
+print("The smallest number in the list is: ", x[0])
+print("The 2nd smallest number in the list is: ", x[1])
+print("The largest number in the list is: ", x[-1])
+print("The smallest number in the list is: ", x[-2])
